4dad5c71de67d35e9d7443338c2bdea6e202daa9
solasun <tako222@naver.com>
2024-05-19 10:16:24 +0900

    - file open

---
+++ ex02/file02.py
@@ -0,0 +1,28 @@
+
+# 변수 = 값 : 메모리 상태
+# name = "길순이"
+
+# open(파일, 모드, 인코딩)
+# 인코딩 생략 = cp949형식
+# 윈도우 시스템에서 path구분 기호 "\" => "\\" 인식
+# 맥, 리눅스, 유닉스, (윈도우사용가능) 등 시스템에서는 경로 "/"
+
+try:
+    #  에러발생할 수 있는 부분
+
+    # 파일변수 (파일핸들러)
+    file1 = open("data\\text01.txt", "r", encoding="UTF-8")
+
+    # 파일객체.read() : 파일 전체 읽기
+    text = file1.read()
+    print(text)
+
+except Exception as e:
+    # 에러 처리 하는 부분
+    print("Error 발생: ",e)
+finally :
+    # 에러와 무관하게 무조건 처리
+    file1.close() # 자원 해제
+
+
+
